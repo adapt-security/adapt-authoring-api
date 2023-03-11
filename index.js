c7f95c2c40c0f537c30493355f37ae675d21c46a
Thomas Taylor <hello@tomtaylor.name>
2023-03-11 11:39:05 +0000

    Remove JSON stringify/parse on update

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
  * Abstract API utilities
  * @namespace api
  */
+export { default as AbstractApiModule } from './lib/AbstractApiModule.js';
+export { default as AbstractApiUtils } from './lib/AbstractApiUtils.js';
 export { default } from './lib/AbstractApiModule.js';

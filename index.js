416aef5f6b5c0ea71f89f9689312562c50988e0f
Thomas Taylor <hello@tomtaylor.name>
2023-03-20 13:07:56 +0000

    Enable eslint and standard ruleset

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/** 
+/**
  * Abstract API utilities
  * @namespace api
  */
-export { default as AbstractApiModule } from './lib/AbstractApiModule.js';
-export { default as AbstractApiUtils } from './lib/AbstractApiUtils.js';
-export { default } from './lib/AbstractApiModule.js';
+export { default as AbstractApiModule } from './lib/AbstractApiModule.js'
+export { default as AbstractApiUtils } from './lib/AbstractApiUtils.js'
+export { default } from './lib/AbstractApiModule.js'
